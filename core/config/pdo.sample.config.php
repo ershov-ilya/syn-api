dc79df37e134ee7ee8c761ec58a76fd9505712b3
ershov-ilya <ershov.ilya@gmail.com>
2015-09-09 15:54:55 +0300

    config

---
+++ core/config/pdo.sample.config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: ershov-ilya
+ * Website: http://ershov.pw/
+ * Date: 06.02.2015
+ * Time: 10:51
+ */
+
+$pdoconfig = array(
+    'dbhost'    =>  'localhost',
+    'dbuser'    =>  'username',
+    'dbpass'    =>  'password',
+    'dbname'    =>  'database',
+    'dbtype'    =>  'mysql'
+);
+
+// Варианты
+// 'dbtype' => 'mysql'
+// 'dbtype' => 'pgsql'
